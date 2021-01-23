60ad38ae4756d62d0db60d24f120dc568c525f04
Nikozs <niko.purola@gmail.com>
2021-01-23 23:36:12 +0200

    Number guessing algorithm

--- src/index.js
+++ src/index.js
@@ -104,3 +104,66 @@ resetGame = () => {
   lastResult.style.backgroundColor = "white";
   randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+let cpuGuessCount = 0;
+
+ComputerPlayer = () => {
+  let keskiarvo = 0;
+  let summa = 0;
+  let maxi = min;
+  let mini = max;
+
+  for (i = 0; i < 1001; i++) {
+    setGameOver();
+    resetGame();
+
+    GuessesHistory = [];
+    var oikearvaus = binary_search(randomNumber, min, max);
+    console.log("Oikea arvaus: ", oikearvaus);
+    console.log("Needed guesses was: ", guessCount);
+    console.log("Guesses: ", GuessesHistory);
+    summa = summa + cpuGuessCount;
+    keskiarvo = summa / i;
+    if (cpuGuessCount > maxi) {
+      maxi = cpuGuessCount;
+    }
+    if (guessCount < mini) {
+      mini = cpuGuessCount;
+    }
+  }
+
+  console.log("AVG: ", keskiarvo);
+  console.log("MIN: ", mini);
+  console.log("MAX: ", maxi);
+};
+
+/*
+Puolitushaun ideana on etsiä etsittävää alkiota taulukon keskeltä, ja mikäli alkiota ei löytynyt, voidaan alkion etsimistä
+jatkaa alkuperäisen taulukon alku- tai loppupään puolivälistä riippuen siitä onko haettava arvo pienempi vai suurempi
+kuin taulukon keskellä oleva alkio.
+
+https://fi.wikipedia.org/wiki/Puolitushaku
+https://en.wikipedia.org/wiki/Binary_search_algorithm
+*/
+binary_search = (key, left, right) => {
+  console.log(key, left, right);
+
+  while (left <= right) {
+    let middle = Math.floor(left + (right - left) / 2);
+    GuessesHistory.push(middle);
+    if (middle < key) {
+      left = middle + 1;
+    } else if (middle > key) {
+      right = middle - 1;
+    } else {
+      return middle;
+    }
+    cpuGuessCount++;
+  }
+  let res = left - 1;
+  return res;
+};
+
+let GuessesHistory = [];
+
+ComputerPlayer();
