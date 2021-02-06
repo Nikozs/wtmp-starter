a9709f5bbfd424d806aa895c35c05e12dcc51a26
Nikozs <niko.purola@gmail.com>
2021-02-07 01:58:31 +0200

    Lunch Menu - PWA

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import SodexoData from './modules/sodexo-data';
 import FazerData from './modules/fazer-data';
 
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 let currentLang = 'fi';
 
 /**
